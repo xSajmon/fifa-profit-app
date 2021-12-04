cf26cacdbca35b2d5be2062e941c7123579c7aee
simon <szymon.stypka@o2.pl>
2021-12-04 15:40:15 +0100

    Initial commit

---
+++ src/main/java/com/example/fifaprofitapp/FifaProfitAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.fifaprofitapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FifaProfitAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FifaProfitAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/fifaprofitapp/FifaprofitappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fifaprofitapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FifaprofitappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
